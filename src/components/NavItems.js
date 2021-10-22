d53668aacea281e87fdc1df7b33f30917ef7fe1f
Trevor Blades <tdblades@gmail.com>
2021-10-22 16:40:43 -0700

    Implement sidebar defaultIsOpen logic

--- src/components/NavItems.js
+++ src/components/NavItems.js
@@ -10,8 +10,23 @@ import {
 import {Link as GatsbyLink} from 'gatsby';
 import {join, relative} from 'path-browserify';
 
+function getFullPath(path, basePath) {
+  return join('/', basePath, path);
+}
+
+function getItemPaths(items, basePath) {
+  return Object.values(items).flatMap(path =>
+    typeof path === 'string'
+      ? getFullPath(path, basePath)
+      : getItemPaths(path, basePath)
+  );
+}
+
 function NavGroup({label, uri, items, basePath}) {
-  const {isOpen, onToggle} = useDisclosure();
+  const itemPaths = getItemPaths(items, basePath);
+  const {isOpen, onToggle} = useDisclosure({
+    defaultIsOpen: itemPaths.some(path => !relative(path, uri))
+  });
   return (
     <div>
       <Button onClick={onToggle}>{label}</Button>
@@ -33,7 +48,7 @@ export default function NavItems({items, uri, basePath}) {
   const activeColor = useColorModeValue('indigo.500', 'indigo.200');
   return Object.entries(items).map(([key, value], index) => {
     if (typeof value === 'string') {
-      const path = join('/', basePath, value);
+      const path = getFullPath(value, basePath);
       const isActive = !relative(path, uri);
       return (
         <div key={index}>
