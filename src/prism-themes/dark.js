3386153f0ecd632bd0092e94413537cc6b77b8ca
Janessa Garrow <janessa@apollographql.com>
2022-02-02 16:24:23 -0700

    Create initial dark theme from apollo midnight vs code theme

---
+++ src/prism-themes/dark.js
@@ -0,0 +1,77 @@
+// TODO: some code blocks don't seem to be getting syntax color styles
+// code block w/ `js{12-17}` language prop doesn't colorize correctly, but `js` language prop does
+
+export const theme = {
+  plain: {
+    color: '#ffffff',
+    backgroundColor: '#060f2f'
+  },
+  styles: [
+    {
+      types: ['comment', 'punctuation'],
+      style: {
+        color: '#798FBB'
+      }
+    },
+    {
+      types: ['variable', 'string'],
+      style: {
+        color: '#FFC18F'
+      }
+    },
+    {
+      types: ['constant'],
+      style: {
+        color: '#FFFFFF'
+      }
+    },
+    {
+      types: ['keyword', 'builtin', 'number', 'char'],
+      style: {
+        color: '#F59140'
+      }
+    },
+    {
+      types: ['tag', 'deleted'],
+      style: {
+        color: '#41D9D3'
+      }
+    },
+    {
+      types: ['function'],
+      style: {
+        color: '#7A92F0'
+      }
+    },
+    {
+      types: ['symbol', 'inserted'],
+      style: {
+        color: '#8BF6F2'
+      }
+    },
+    {
+      types: ['changed'],
+      style: {
+        color: '#FFCB6B'
+      }
+    },
+    {
+      types: ['attr-name'],
+      style: {
+        color: '#C792EA'
+      }
+    },
+    {
+      types: ['regex'],
+      style: {
+        color: '#89DDFF'
+      }
+    },
+    {
+      types: ['boolean'],
+      style: {
+        color: '#F4D03F'
+      }
+    }
+  ]
+};
