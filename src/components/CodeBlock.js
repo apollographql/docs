76d271c857eb433541c0afcbf0225a2bd771ab38
Janessa Garrow <janessa@apollographql.com>
2022-02-09 12:14:01 -0700

    Add optional line numbers for code blocks

--- src/components/CodeBlock.js
+++ src/components/CodeBlock.js
@@ -32,14 +32,15 @@ import 'prismjs/components/prism-tsx';
 import 'prismjs/components/prism-typescript';
 
 export const CodeBlockContext = createContext();
-
+const SPACING = 4;
 export default function CodeBlock({children}) {
   const [child] = Array.isArray(children) ? children : [children];
   const {
     className = 'language-text',
     children: innerChildren,
     metastring,
-    'data-meta': dataMeta
+    'data-meta': dataMeta,
+    showLineNumbers = true
   } = child.props;
 
   const meta = metastring || dataMeta;
@@ -55,6 +56,7 @@ export default function CodeBlock({children}) {
   const highlightColor = useColorModeValue('gray.100', 'blackAlpha.400');
   const borderColor = useColorModeValue('gray.200', 'gray.700');
   const titleBgColor = useColorModeValue('white', 'inherit');
+  const lineNumberColor = useColorModeValue('gray.300', '#798fbb');
   const languageMenu = useContext(CodeBlockContext);
 
   return (
@@ -64,58 +66,88 @@ export default function CodeBlock({children}) {
       code={code.trim()}
       language={className.replace(/^language-/, '')}
     >
-      {({className, style, tokens, getLineProps, getTokenProps}) => (
-        <Box
-          rounded="md"
-          style={style}
-          pos="relative"
-          shadow="sm"
-          borderWidth="1px"
-          borderColor={borderColor}
-        >
-          <Box fontSize="md" fontFamily="mono">
-            {title && (
-              <Box
-                px="4"
-                py="2"
-                borderBottomWidth="1px"
-                bgColor={titleBgColor}
-                borderTopRadius="md"
-              >
-                {title}
-              </Box>
-            )}
-            <chakra.pre
-              className={className}
-              py="4"
-              fontFamily="inherit"
-              overflow="auto"
-            >
-              {tokens.map((line, i) => (
+      {({className, style, tokens, getLineProps, getTokenProps}) => {
+        // length of longest line number
+        // ex. if there are 28 lines in the code block, lineNumberOffset = 2ch
+        const lineNumberOffset = tokens.length.toString().length + 'ch';
+        return (
+          <Box
+            rounded="md"
+            style={style}
+            pos="relative"
+            shadow="sm"
+            borderWidth="1px"
+            borderColor={borderColor}
+          >
+            <Box fontSize="md" fontFamily="mono">
+              {title && (
                 <Box
-                  key={i}
-                  {...getLineProps({
-                    line,
-                    key: i
-                  })}
-                  px="4"
-                  bg={linesToHighlight.includes(i + 1) && highlightColor}
+                  px={SPACING}
+                  py="2"
+                  borderBottomWidth="1px"
+                  bgColor={titleBgColor}
+                  borderTopRadius="md"
                 >
-                  {line.map((token, key) => (
-                    <span key={key} {...getTokenProps({token, key})} />
-                  ))}
+                  {title}
                 </Box>
-              ))}
-            </chakra.pre>
+              )}
+              <chakra.pre
+                className={className}
+                py={SPACING}
+                px={showLineNumbers && SPACING}
+                fontFamily="inherit"
+                overflow="auto"
+              >
+                {tokens.map((line, i) => (
+                  <Box
+                    {...getLineProps({
+                      line,
+                      key: i
+                    })}
+                    key={i}
+                    pl={(SPACING / 2) * (showLineNumbers ? 2 : 1)}
+                    bg={linesToHighlight.includes(i + 1) && highlightColor}
+                  >
+                    <Box ml={showLineNumbers && lineNumberOffset}>
+                      {line.map((token, key) => (
+                        <span key={key} {...getTokenProps({token, key})} />
+                      ))}
+                    </Box>
+                  </Box>
+                ))}
+              </chakra.pre>
+              {/* put below all code and then position so that if user selects text, line numbers will be excluded */}
+              {showLineNumbers && (
+                <chakra.pre
+                  aria-hidden="true" // hide from screen reader
+                  pos="absolute"
+                  top={title ? '57px' : SPACING} // 57px = SPACING (16px) + height of title box (41px)
+                  left={SPACING}
+                  textAlign="right"
+                  bgColor={theme.plain.backgroundColor} // for horizontal scrolling of code text
+                >
+                  {tokens.map((_, index) => (
+                    <Box
+                      key={index}
+                      w={lineNumberOffset}
+                      mr={SPACING}
+                      color={lineNumberColor}
+                    >
+                      {index + 1}
+                    </Box>
+                  ))}
+                </chakra.pre>
+              )}
+            </Box>
+            <ButtonGroup size="xs" pos="absolute" top="2" right="2">
+              <Button leftIcon={<FiClipboard />} onClick={onCopy}>
+                {hasCopied ? 'Copied!' : 'Copy'}
+              </Button>
+              {languageMenu}
+            </ButtonGroup>
           </Box>
-          <ButtonGroup size="xs" pos="absolute" top="2" right="2">
-            <Button leftIcon={<FiClipboard />} onClick={onCopy}>
-              {hasCopied ? 'Copied!' : 'Copy'}
-            </Button>
-            {languageMenu}
-          </ButtonGroup>
-        </Box>
-      )}
+        );
+      }}
     </Highlight>
   );
 }
