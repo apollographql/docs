847c7de2177ea86fda75d0ad07e984f9a0998582
Trevor Blades <tdblades@gmail.com>
2022-02-17 14:55:14 -0800

    Report on broken links

--- plugins/gatsby-plugin-check-links/gatsby-node.js
+++ plugins/gatsby-plugin-check-links/gatsby-node.js
@@ -5,11 +5,11 @@ const {dirname, resolve, isAbsolute} = require('path');
 
 const TRAILING_SLASH_PATTERN = /(?!^)\/$/;
 
-exports.createPages = async ({graphql}) => {
+exports.createPages = async ({graphql, reporter}) => {
   const {data} = await graphql(
     `
       {
-        allFile(filter: {extension: {in: ["md", "mdx"]}}, limit: 100) {
+        allFile(filter: {extension: {in: ["md", "mdx"]}}) {
           nodes {
             name
             children {
@@ -50,8 +50,8 @@ exports.createPages = async ({graphql}) => {
     const links = [];
     const tree = child.mdxAST || fromMarkdown(child.rawMarkdownBody);
     visit(tree, 'link', ({url, position}) => {
-      // is a hash or not a URL
-      if (url.startsWith('#') || !/^https?:\/\//.test(url)) {
+      // is a hash or not a URL with a protocol (http, mailto, etc.)
+      if (url.startsWith('#') || !/^\w+:/.test(url)) {
         const {line, column} = position.start;
         links.push({
           to: url,
@@ -72,10 +72,11 @@ exports.createPages = async ({graphql}) => {
     };
   }, {});
 
-  const validLinks = [];
-  const brokenLinks = [];
+  let totalBrokenLinks = 0;
 
   for (const slug in pages) {
+    const brokenLinks = [];
+
     const {name, links} = pages[slug];
     for (const link of links) {
       const fullPath = link.to.startsWith('#')
@@ -88,14 +89,33 @@ exports.createPages = async ({graphql}) => {
       const trimmedPath = absPath.replace(TRAILING_SLASH_PATTERN, '');
       const target = pages[trimmedPath];
 
-      if (target && (!heading || target.headings.includes(heading))) {
-        validLinks.push(fullPath);
-        continue;
+      // if the target doesn't exist or the heading hash is wrong
+      if (!target || (heading && !target.headings.includes(heading))) {
+        brokenLinks.push(link);
+      }
+    }
+
+    if (brokenLinks.length) {
+      reporter.warn(`${brokenLinks.length} broken links found on ${slug}`);
+
+      const lineDigits = brokenLinks.map(link => link.line.toString().length);
+      const maxLineDigits = Math.max(...lineDigits);
+      const columnDigits = brokenLinks.map(
+        link => link.column.toString().length
+      );
+      const maxColumnDigits = Math.max(...columnDigits);
+
+      for (const link of brokenLinks) {
+        const position = [
+          link.line.toString().padStart(maxLineDigits, ' '),
+          link.column.toString().padEnd(maxColumnDigits, ' ')
+        ].join(':');
+        reporter.warn(`${position} ${link.to}`);
       }
 
-      brokenLinks.push(fullPath);
+      totalBrokenLinks += brokenLinks.length;
     }
   }
 
-  console.log({validLinks, brokenLinks});
+  reporter.warn(`${totalBrokenLinks} total broken links`);
 };
