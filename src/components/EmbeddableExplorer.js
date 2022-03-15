edea86f1fdba7c5fb002bea552b5677c07e84d98
mayakoneval <mayakoneval@gmail.com>
2022-03-15 14:36:10 -0700

    use npm react apollo explorer

--- src/components/EmbeddableExplorer.js
+++ src/components/EmbeddableExplorer.js
@@ -1,71 +1,51 @@
-import PropTypes from 'prop-types';
-import React, {useEffect, useRef} from 'react';
-import {Box, useColorMode} from '@chakra-ui/react';
+import React from "react";
+import { Box, useColorMode } from "@chakra-ui/react";
+import { ApolloExplorerReact } from "@apollo/explorer";
 
-export default function EmbeddableExplorer({
-  graphRef = 'Apollo-Fullstack-Demo-o3tsz8@current',
-  endpointUrl = 'https://apollo-fullstack-tutorial.herokuapp.com/graphql',
-  document = `
-    query GetLaunches {
+const EmbeddableExplorer = ({
+  graphRef = "Apollo-Fullstack-Demo-o3tsz8@current",
+  endpointUrl = "https://apollo-fullstack-tutorial.herokuapp.com/graphql",
+  initialDocument = `query GetLaunches {
+    launches {
       launches {
-        launches {
+        id
+        site
+        rocket {
           id
-          site
-          rocket {
-            id
-            name
-          }
+          name
         }
       }
     }
-  `
-}) {
-  const containerRef = useRef();
-  const {colorMode} = useColorMode();
-
-  useEffect(() => {
-    // create a script element whose src = external script src from Explorer embed
-    // TODO: check if there's already an embed explorer script so we don't add extra ones unnecessarily
-    const script = window.document.createElement('script');
-    script.src =
-      'https://embeddable-explorer.cdn.apollographql.com/_latest/embeddable-explorer.umd.production.min.js';
-    script.async = true;
-
-    // add the script to the body
-    window.document.body.appendChild(script);
-
-    // create new instance of EmbeddedExplorer
-    const target = containerRef.current;
-    const onLoad = () =>
-      new window.EmbeddedExplorer({
-        graphRef,
-        endpointUrl,
-        initialState: {
-          document,
-          theme: colorMode
+  }`,
+}) => {
+  const { colorMode } = useColorMode();
+  return (
+    <Box
+      w="full"
+      h={450}
+      border="none"
+      rounded="md"
+      sx={{
+        ".embed": {
+          width: "100%",
+          height: "100%",
         },
-        target
-      });
-
-    // we need to wait for the external script to load first before configuring our instance of EmbeddedExplorer
-    script.addEventListener('load', onLoad);
-
-    // clean up script tag and event listener
-    return () => {
-      // remove iframe that Studio Explorer appends to our div w/ an `id` attribute
-      // to prevent additional iframes from being added (i.e. in local dev w/ hot reloading)
-      target.firstChild.remove();
-
-      script.removeEventListener('load', onLoad);
-      window.document.body.removeChild(script);
-    };
-  }, [graphRef, endpointUrl, document, colorMode]);
-
-  return <Box ref={containerRef} w="full" h={450} border="none" rounded="md" />;
-}
-
-EmbeddableExplorer.propTypes = {
-  graphRef: PropTypes.string,
-  endpointUrl: PropTypes.string,
-  document: PropTypes.string
+      }}
+    >
+      <ApolloExplorerReact
+        className="embed"
+        endpointUrl={endpointUrl}
+        graphRef={graphRef}
+        persistExplorerState={false}
+        initialState={{
+          displayOptions: {
+            theme: colorMode,
+          },
+          document: initialDocument,
+        }}
+      />
+    </Box>
+  );
 };
+
+export default EmbeddableExplorer;
