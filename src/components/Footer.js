64ad45e85c37ada95c1319790feea00108a94552
Xander Low <xander@xandydandy.com>
2022-03-28 22:35:48 -0700

    imported category defaults from chakra-helpers

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,32 +11,45 @@ import {
   SimpleGrid,
   Text
 } from '@chakra-ui/react';
+import {
+  communityCategory,
+  companyCategory,
+  helpCategory,
+  productCategory
+} from '@apollo/chakra-helpers';
 
-const Resources = {
-  'GraphQL Tutorials': 'https://odyssey.apollographql.com',
-  Docs: 'https://www.apollographql.com/docs',
-  Blog: 'https://www.apollographql.com/blog',
-  'Developer Hub': 'https://www.apollographql.com/developers',
-  Community: 'https://community.apollographql.com'
-};
+// const Resources = {
+//   'GraphQL Tutorials': 'https://odyssey.apollographql.com',
+//   Docs: 'https://www.apollographql.com/docs',
+//   Blog: 'https://www.apollographql.com/blog',
+//   'Developer Hub': 'https://www.apollographql.com/developers',
+//   Community: 'https://community.apollographql.com'
+// };
 
-const Company = {
-  'About Apollo': 'https://www.apollographql.com',
-  Careers: 'https://www.apollographql.com/careers',
-  Team: 'https://www.apollographql.com/team',
-  Studio: 'https://studio.apollographql.com',
-  Twitter: 'https://twitter.com/apollographql'
-};
+// const Company = {
+//   'About Apollo': 'https://www.apollographql.com',
+//   Careers: 'https://www.apollographql.com/careers',
+//   Team: 'https://www.apollographql.com/team',
+//   Studio: 'https://studio.apollographql.com',
+//   Twitter: 'https://twitter.com/apollographql'
+// };
 
-const Help = {
-  'Contact an Expert': 'https://www.apollographql.com/contact-sales',
-  'Get Support': 'https://www.apollographql.com/support',
-  'Website Terms of Service':
-    'https://www.apollographql.com/Apollo-Website-Terms-of-Service.pdf',
-  'Product Terms of Service':
-    'https://www.apollographql.com/Apollo-Terms-of-Service.pdf',
-  'Privacy Policy': 'https://www.apollographql.com/privacy-policy'
-};
+// const Help = {
+//   'Contact an Expert': 'https://www.apollographql.com/contact-sales',
+//   'Get Support': 'https://www.apollographql.com/support',
+//   'Website Terms of Service':
+//     'https://www.apollographql.com/Apollo-Website-Terms-of-Service.pdf',
+//   'Product Terms of Service':
+//     'https://www.apollographql.com/Apollo-Terms-of-Service.pdf',
+//   'Privacy Policy': 'https://www.apollographql.com/privacy-policy'
+// };
+
+const categories = [
+  helpCategory,
+  communityCategory,
+  companyCategory,
+  productCategory
+];
 
 export default function Footer({navConfig = {Resources, Company, Help}}) {
   return (
@@ -56,15 +69,31 @@ export default function Footer({navConfig = {Resources, Company, Help}}) {
         </Flex>
         <Text>&copy; Apollo Graph Inc.</Text>
       </div>
-      {Object.entries(navConfig).map(([label, items], index) => (
+      {/* {Object.entries(navConfig).map(([label, items], index) => (
         <div key={index}>
           <Heading mb="2" size="md">
             {label}
           </Heading>
           <List spacing="1">
-            {Object.entries(items).map(([label, url], index) => (
+          {Object.entries(items).map(([label, url], index) => (
+            <ListItem key={index}>
+            <Link href={url}>{label}</Link>
+            </ListItem>
+            ))}
+            </List>
+            </div>
+          ))} */}
+      {categories.map(({links, title}, index) => (
+        <div key={index}>
+          <Heading mb="2" size="md">
+            {title}
+          </Heading>
+          <List spacing="1">
+            {links.map(({href, text, internal}, index) => (
               <ListItem key={index}>
-                <Link href={url}>{label}</Link>
+                <Link href={href} isExternal={!internal}>
+                  {text}
+                </Link>
               </ListItem>
             ))}
           </List>
