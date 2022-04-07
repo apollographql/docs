a5305975e7d2ff053e0247e7aafb4dfdde11937e
Trevor Blades <tdblades@gmail.com>
2022-04-06 19:31:19 -0700

    Export components from the main entrypoint

--- packages/chakra-helpers/src/index.ts
+++ packages/chakra-helpers/src/index.ts
@@ -1,3 +1,5 @@
 export * from './helpers';
 export {defaultConfig as footerConfig} from './footer';
 export {EmbeddableExplorer} from './EmbeddableExplorer';
+export {CodeBlock} from './CodeBlock';
+export {MultiCodeBlock} from './MultiCodeBlock';
