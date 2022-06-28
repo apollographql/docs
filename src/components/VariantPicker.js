76f210703619781edce6724ad9d02335124aa53c
Stephen Barlow <stephen@apollographql.com>
2022-06-27 17:27:18 -0700

    Replace unwieldy nested lists with unwieldy selects

--- src/components/VariantPicker.js
+++ src/components/VariantPicker.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {gql, useQuery} from '@apollo/client';
 
 const GET_VISIBLE_VARIANTS = gql`
@@ -23,37 +23,57 @@ const GET_VISIBLE_VARIANTS = gql`
 `;
 
 export default function VariantPicker() {
+  const [currentOrg, setCurrentOrg] = useState(null);
+  const [currentGraph, setCurrentGraph] = useState(null);
   const {loading, error, data} = useQuery(GET_VISIBLE_VARIANTS);
+  const handleOrgChange = event => {
+    setCurrentOrg(orgs.find(org => org.id === event.target.value));
+  };
+  const handleGraphChange = event => {
+    setCurrentGraph(
+      currentOrg.services.find(graph => graph.id === event.target.value)
+    );
+  };
+
   if (loading) return 'Loading...';
   if (error) return error.message;
   if (!data.me) return 'Not logged in!';
   const orgs = data.me.memberships.map(membership => membership.account);
-  console.log(orgs);
-  const allVariants = orgs.map(org => {
+  const orgOptions = orgs.map(org => {
     return (
-      <li key={org.id}>
-        <strong>{org.name}</strong>
-        <ul>
-          {org.services.map(graph => {
-            return (
-              <li key={graph.id}>
-                <code>{graph.id}</code>
-                <ul>
-                  {graph.variants.map(variant => {
-                    return (
-                      <li key={variant.name}>
-                        <code>@{variant.name}</code>
-                      </li>
-                    );
-                  })}
-                </ul>
-              </li>
-            );
-          })}
-        </ul>
-      </li>
+      <option key={org.id} value={org.id}>
+        {org.name}
+      </option>
     );
   });
-  console.log(allVariants);
-  return <ul>{allVariants}</ul>;
+
+  let graphOptions = [];
+  if (currentOrg) {
+    graphOptions = currentOrg.services.map(graph => {
+      return (
+        <option key={graph.id} value={graph.id}>
+          {graph.id}
+        </option>
+      );
+    });
+  }
+
+  let variantOptions = [];
+  if (currentGraph) {
+    variantOptions = currentGraph.variants.map(variant => {
+      return (
+        <option key={variant.name} value={variant.name}>
+          @{variant.name}
+        </option>
+      );
+    });
+  }
+
+  return (
+    <>
+      <select onChange={handleOrgChange}>{orgOptions}</select>
+      <select onChange={handleGraphChange}>{graphOptions}</select>
+      <select>{variantOptions}</select>
+    </>
+  );
 }
