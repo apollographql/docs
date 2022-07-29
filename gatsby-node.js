d13a83023123cd2f159394ceb4e6d1c3ded5581f
Trevor Blades <tdblades@gmail.com>
2022-07-28 17:20:09 -0700

    Restore old docs.json URL

--- gatsby-node.js
+++ gatsby-node.js
@@ -15,7 +15,7 @@ exports.sourceNodes = ({
 }) =>
   // download Apollo Client typedoc output and save it as a file node
   createRemoteFileNode({
-    url: 'https://tb-typedoc-version--apollo-client-docs.netlify.app/docs.json',
+    url: 'https://apollo-client-docs.netlify.app/docs.json',
     store,
     cache,
     createNode,
