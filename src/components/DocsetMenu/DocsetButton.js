5f8a5ce422a2c83ac4efdae3f693d4c2a2d81eee
Trevor Blades <tdblades@gmail.com>
2022-10-21 16:51:42 -0700

    Remove unused import and ?

--- src/components/DocsetMenu/DocsetButton.js
+++ src/components/DocsetMenu/DocsetButton.js
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import React, {useContext} from 'react';
 import path from 'path';
-import {Button, Link, Tooltip} from '@chakra-ui/react';
+import {Button, Tooltip} from '@chakra-ui/react';
 import {Link as GatsbyLink} from 'gatsby';
 import {PathContext} from '../../utils';
 
@@ -9,7 +9,7 @@ export default function DocsetButton({to, tooltipLabel, ...props}) {
   const {basePath} = useContext(PathContext);
   const [docsetPath] = basePath.split(path.sep);
 
-  const linkProps = to?.startsWith('/')
+  const linkProps = to.startsWith('/')
     ? {
         as: GatsbyLink,
         to
