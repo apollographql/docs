8ffb422bc54e5a829389814302b6db3b0e11b42e
Trevor Blades <tdblades@gmail.com>
2023-02-23 18:20:15 -0800

    Refactor left sidebar to use components instead of config file

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,17 +1,22 @@
 import PropTypes from 'prop-types';
 import React, {useContext, useEffect, useRef, useState} from 'react';
+import {AiOutlineHome} from '@react-icons/all-files/ai/AiOutlineHome';
 import {
   Box,
-  Icon,
-  List,
-  ListItem,
+  Stack,
+  StackDivider,
   chakra,
   useColorModeValue
 } from '@chakra-ui/react';
+import {DOCSET_ICONS} from '../DocsetMenu';
+import {
+  DocsetContext,
+  SidebarCategory,
+  SidebarCategoryLink
+} from './SidebarCategory';
 import {PathContext} from '../../utils';
 import {SidebarNav} from './SidebarNav';
 import {TOTAL_HEADER_HEIGHT} from '../Header';
-import {mockDocset, sidebarMenu} from './sidebar.config';
 
 const SIDEBAR_WIDTH = 280;
 const COLLAPSED_SIDEBAR_WIDTH = 85;
@@ -21,13 +26,7 @@ export const PAGE_SIDEBAR_MARGIN = SIDEBAR_WIDTH + COLLAPSED_SIDEBAR_WIDTH;
 export const SIDEBAR_WIDTH_BASE = 450;
 export const SIDEBAR_WIDTH_XL = 500;
 
-const {categories} = sidebarMenu;
-
 export function Sidebar({children, configs, isHidden}) {
-  configs = {
-    ...configs,
-    ...mockDocset
-  };
   const sidebarRef = useRef();
 
   const pathContext = useContext(PathContext);
@@ -62,7 +61,6 @@ export function Sidebar({children, configs, isHidden}) {
       zIndex="1"
       transitionProperty="visibility, opacity, transform"
       transitionDuration="normal"
-      sx={{scrollPaddingTop: 12}}
       css={{top: TOTAL_HEADER_HEIGHT}}
       style={{
         visibility: isHidden ? 'hidden' : 'visible',
@@ -77,6 +75,7 @@ export function Sidebar({children, configs, isHidden}) {
       <Box
         w={SIDEBAR_WIDTH}
         color="white"
+        fontWeight="semibold"
         bgColor={leftNavBgColor}
         flexShrink="0"
         overflow="auto"
@@ -84,81 +83,79 @@ export function Sidebar({children, configs, isHidden}) {
           setSidebarOpen(true);
 
           if (event.target === event.currentTarget) {
+            // TODO: this doesn't work when the mouse is over the scrollbar
             setActiveDocset(null);
           }
         }}
       >
-        <List p="4">
-          {categories.map(category => {
-            return (
-              <ListItem
-                fontSize="13px"
-                fontFamily="mono"
-                key={category.name}
-                color="#fff"
-                mb="2"
-                sx={{
-                  '.hide-when-closed': {
-                    opacity: sidebarOpen ? '100' : '0'
-                  }
-                }}
+        <DocsetContext.Provider
+          value={{
+            activeDocset,
+            setActiveDocset,
+            sidebarOpen
+          }}
+        >
+          <Stack
+            p="4"
+            spacing="4"
+            divider={<StackDivider borderColor="whiteAlpha.300" />}
+          >
+            <SidebarCategory title="Start">
+              <SidebarCategoryLink docset="/" icon={<AiOutlineHome />}>
+                Home
+              </SidebarCategoryLink>
+            </SidebarCategory>
+            <SidebarCategory title="GraphOS">
+              <SidebarCategoryLink docset="graphos" icon={DOCSET_ICONS.graphos}>
+                Building Graphs
+              </SidebarCategoryLink>
+              <SidebarCategoryLink
+                docset="federation"
+                icon={DOCSET_ICONS.federation}
               >
-                <chakra.span mb="2" display="block">
-                  {category.name}
-                </chakra.span>
-                {category.items.map(item => {
-                  const {docset} = item;
-                  const isActiveMenu = activeDocset === docset;
-                  const isActivePath = pathContext.basePath === docset;
-                  return (
-                    <ListItem
-                      fontFamily="body"
-                      key={item.title}
-                      py="2"
-                      px="4"
-                      display="flex"
-                      alignItems="center"
-                      onFocus={() => setActiveDocset(docset)}
-                      onMouseEnter={() => setActiveDocset(docset)}
-                      fontSize="sm"
-                      fontWeight="semibold"
-                      letterSpacing="wider"
-                      position="relative"
-                      _after={{
-                        content: '""',
-                        display: isActivePath ? 'block' : 'none',
-                        width: '4',
-                        bg: 'purple.500',
-                        position: 'absolute',
-                        left: '100%',
-                        top: 0,
-                        bottom: 0
-                      }}
-                      borderRadius="md"
-                      borderRightRadius={isActivePath ? 0 : undefined}
-                      bg={
-                        isActivePath
-                          ? 'purple.500'
-                          : isActiveMenu
-                          ? 'whiteAlpha.300'
-                          : 'inherit'
-                      }
-                      tabIndex="0"
-                    >
-                      <Icon w="5" h="5" mr="2" as={item.icon} />{' '}
-                      <chakra.span
-                        transition="opacity ease-in-out 100ms"
-                        opacity={sidebarOpen ? '100' : '0'}
-                      >
-                        {item.title}
-                      </chakra.span>
-                    </ListItem>
-                  );
-                })}
-              </ListItem>
-            );
-          })}
-        </List>
+                Federation
+              </SidebarCategoryLink>
+              <SidebarCategoryLink
+                docset="technotes"
+                icon={DOCSET_ICONS.technotes}
+              >
+                Tech Notes
+              </SidebarCategoryLink>
+            </SidebarCategory>
+            <SidebarCategory title="Tools &amp; Libraries">
+              <SidebarCategoryLink docset="rover" icon={DOCSET_ICONS.rover}>
+                Rover CLI
+              </SidebarCategoryLink>
+              <SidebarCategoryLink docset="router" icon={DOCSET_ICONS.router}>
+                Apollo Router
+              </SidebarCategoryLink>
+              <SidebarCategoryLink
+                docset="apollo-server"
+                icon={DOCSET_ICONS['apollo-server']}
+              >
+                Apollo Server
+              </SidebarCategoryLink>
+              <SidebarCategoryLink
+                docset="react"
+                icon={DOCSET_ICONS['apollo-client']}
+              >
+                Client (React / JS)
+              </SidebarCategoryLink>
+              <SidebarCategoryLink
+                docset="kotlin"
+                icon={DOCSET_ICONS['apollo-kotlin']}
+              >
+                Client (Kotlin)
+              </SidebarCategoryLink>
+              <SidebarCategoryLink
+                docset="ios"
+                icon={DOCSET_ICONS['apollo-ios']}
+              >
+                Client (iOS)
+              </SidebarCategoryLink>
+            </SidebarCategory>
+          </Stack>
+        </DocsetContext.Provider>
       </Box>
       <Box
         w={SIDEBAR_WIDTH}
@@ -170,7 +167,9 @@ export function Sidebar({children, configs, isHidden}) {
             ? 'none'
             : `translateX(-${SIDEBAR_WIDTH - COLLAPSED_SIDEBAR_WIDTH}px)`
         }
-        transition="transform ease-in-out 100ms"
+        transitionProperty="transform"
+        transitionDuration="normal"
+        transitionTimingFunction="ease-in-out"
         overflow="auto"
       >
         {activeDocset ? (
@@ -181,6 +180,7 @@ export function Sidebar({children, configs, isHidden}) {
             }}
           >
             <SidebarNav
+              key={activeDocset}
               docset={configs[activeDocset].docset}
               navItems={configs[activeDocset].navItems}
             />
