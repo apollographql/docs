ce310429af9e1c898e22e2a19f60a270ac25da91
Trevor Blades <trevorblades@Trevors-MacBook-Pro.local>
2023-03-09 13:59:32 -0800

    Add dummy version selector and prevent sidebar close bug when clicking an icon

--- src/components/Sidebar/NavItems.js
+++ src/components/Sidebar/NavItems.js
@@ -136,7 +136,10 @@ function NavGroup({group, depth}) {
         }}
       >
         <span>{group.title}</span>
-        {isOpen ? <FiChevronDown /> : <FiChevronRight />}
+        <Box
+          as={isOpen ? FiChevronDown : FiChevronRight}
+          pointerEvents="none"
+        />
       </HStack>
       <Collapse unmountOnExit in={isOpen}>
         <NavItems
--- src/components/Sidebar/SidebarNav.js
+++ src/components/Sidebar/SidebarNav.js
@@ -2,8 +2,20 @@ import NavItems, {GA_EVENT_CATEGORY_SIDEBAR, NavContext} from './NavItems';
 import PropTypes from 'prop-types';
 import React, {useMemo} from 'react';
 import useLocalStorage from 'react-use/lib/useLocalStorage';
+import {
+  Box,
+  Button,
+  Flex,
+  HStack,
+  Menu,
+  MenuButton,
+  MenuItemOption,
+  MenuList,
+  MenuOptionGroup,
+  chakra
+} from '@chakra-ui/react';
 import {BsChevronContract, BsChevronExpand} from 'react-icons/bs';
-import {Button, Flex, Heading, chakra} from '@chakra-ui/react';
+import {FiChevronDown} from 'react-icons/fi';
 import {flattenNavItems} from '../../utils';
 
 export function SidebarNav({docset, navItems}) {
@@ -50,35 +62,26 @@ export function SidebarNav({docset, navItems}) {
     <>
       <Flex p="4" pos="sticky" top="0" zIndex="1" bg="bg">
         {/* TODO: the docset title below should match the title in the left sidebar */}
-        <Heading size="md">{docset}</Heading>
-        {navGroups.length > 0 && (
-          <Button
-            ml="auto"
-            size="xs"
-            variant="ghost"
-            leftIcon={
-              isAllExpanded ? <BsChevronContract /> : <BsChevronExpand />
-            }
-            onClick={() => {
-              const expanded = !isAllExpanded;
-              setLocalNavState(
-                navGroups.reduce(
-                  (acc, group) => ({
-                    ...acc,
-                    [group.id]: expanded
-                  }),
-                  {}
-                )
-              );
-              window.gtag?.('event', 'Toggle all', {
-                event_category: GA_EVENT_CATEGORY_SIDEBAR,
-                event_label: expanded ? 'expand' : 'collapse'
-              });
-            }}
+        <chakra.h2 mr="auto" fontSize="xl" fontWeight="semibold">
+          {docset}
+        </chakra.h2>
+        <Menu>
+          <MenuButton
+            size="sm"
+            ml="2"
+            variant="outline"
+            rightIcon={<FiChevronDown />}
+            as={Button}
           >
-            {isAllExpanded ? 'Collapse' : 'Expand'} all
-          </Button>
-        )}
+            v1
+          </MenuButton>
+          <MenuList>
+            <MenuOptionGroup value="v1">
+              <MenuItemOption value="v1">v1</MenuItemOption>
+              <MenuItemOption value="v2">v2</MenuItemOption>
+            </MenuOptionGroup>
+          </MenuList>
+        </Menu>
       </Flex>
       <NavContext.Provider
         value={{
@@ -87,6 +90,37 @@ export function SidebarNav({docset, navItems}) {
         }}
       >
         <chakra.nav px="4">
+          {navGroups.length > 0 && (
+            <HStack
+              as="button"
+              fontSize="sm"
+              fontWeight="semibold"
+              spacing="1"
+              mb="4"
+              onClick={() => {
+                const expanded = !isAllExpanded;
+                setLocalNavState(
+                  navGroups.reduce(
+                    (acc, group) => ({
+                      ...acc,
+                      [group.id]: expanded
+                    }),
+                    {}
+                  )
+                );
+                window.gtag?.('event', 'Toggle all', {
+                  event_category: GA_EVENT_CATEGORY_SIDEBAR,
+                  event_label: expanded ? 'expand' : 'collapse'
+                });
+              }}
+            >
+              <span>{isAllExpanded ? 'Collapse' : 'Expand'} all</span>
+              <Box
+                as={isAllExpanded ? BsChevronContract : BsChevronExpand}
+                pointerEvents="none"
+              />
+            </HStack>
+          )}
           <NavItems items={navItems} />
         </chakra.nav>
       </NavContext.Provider>
