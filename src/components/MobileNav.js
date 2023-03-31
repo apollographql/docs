a4f7c688e120ca63e2d930e11bad2ac7bf7c5d9a
Trevor Blades <tdblades@gmail.com>
2023-03-31 16:42:53 -0700

    Reset mobile nav state when the nav closes/opens again

--- src/components/MobileNav.js
+++ src/components/MobileNav.js
@@ -3,6 +3,7 @@ import React, {useContext, useState} from 'react';
 import {
   Box,
   Drawer,
+  DrawerCloseButton,
   DrawerContent,
   DrawerOverlay,
   IconButton,
@@ -12,10 +13,56 @@ import {DocsetContext, LeftSidebarNav, SidebarNav} from './Sidebar';
 import {FiMenu} from 'react-icons/fi';
 import {PathContext} from '../utils';
 
-export default function MobileNav({configs}) {
-  const {isOpen, onOpen, onClose} = useDisclosure();
+function MobileNavContent({configs}) {
   const {basePath, ...pathContext} = useContext(PathContext);
   const [activeDocset, setActiveDocset] = useState(basePath);
+  return (
+    <>
+      {!activeDocset && <DrawerCloseButton zIndex="1" top="3" color="white" />}
+      <Box overflow="auto" pos="relative" zIndex="0">
+        <DocsetContext.Provider
+          value={{
+            configs,
+            activeDocset,
+            setActiveDocset,
+            sidebarOpen: true,
+            clickToSelect: true
+          }}
+        >
+          {activeDocset ? (
+            <PathContext.Provider
+              value={{
+                ...pathContext,
+                basePath: `/${activeDocset}`
+              }}
+            >
+              <SidebarNav
+                key={activeDocset}
+                currentVersion={configs[activeDocset].currentVersion}
+                versions={configs[activeDocset].versions}
+                docset={configs[activeDocset].docset}
+                navItems={configs[activeDocset].navItems}
+                onGoBack={() => setActiveDocset(null)}
+                onVersionChange={version => {
+                  setActiveDocset(version.slug);
+                }}
+              />
+            </PathContext.Provider>
+          ) : (
+            <LeftSidebarNav />
+          )}
+        </DocsetContext.Provider>
+      </Box>
+    </>
+  );
+}
+
+MobileNavContent.propTypes = {
+  configs: PropTypes.object.isRequired
+};
+
+export default function MobileNav({configs}) {
+  const {isOpen, onOpen, onClose} = useDisclosure();
 
   return (
     <>
@@ -30,40 +77,7 @@ export default function MobileNav({configs}) {
       <Drawer placement="left" isOpen={isOpen} onClose={onClose}>
         <DrawerOverlay />
         <DrawerContent>
-          <Box overflow="auto" pos="relative" zIndex="0">
-            <DocsetContext.Provider
-              value={{
-                configs,
-                activeDocset,
-                setActiveDocset,
-                sidebarOpen: true,
-                clickToSelect: true
-              }}
-            >
-              {activeDocset ? (
-                <PathContext.Provider
-                  value={{
-                    ...pathContext,
-                    basePath: `/${activeDocset}`
-                  }}
-                >
-                  <SidebarNav
-                    key={activeDocset}
-                    currentVersion={configs[activeDocset].currentVersion}
-                    versions={configs[activeDocset].versions}
-                    docset={configs[activeDocset].docset}
-                    navItems={configs[activeDocset].navItems}
-                    onGoBack={() => setActiveDocset(null)}
-                    onVersionChange={version => {
-                      setActiveDocset(version.slug);
-                    }}
-                  />
-                </PathContext.Provider>
-              ) : (
-                <LeftSidebarNav />
-              )}
-            </DocsetContext.Provider>
-          </Box>
+          <MobileNavContent configs={configs} />
         </DrawerContent>
       </Drawer>
     </>
