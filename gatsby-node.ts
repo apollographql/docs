00f7d5b30c067f6aa294e24e11cf0d7f095ec0e0
Trevor Blades <tdblades@gmail.com>
2023-04-27 17:11:50 -0700

    Type gatsby node hook

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,6 +1,11 @@
 import { createFilePath } from "gatsby-source-filesystem";
+import type { GatsbyNode } from "gatsby";
 
-export const onCreateNode = ({ node, getNode, actions }) => {
+export const onCreateNode: GatsbyNode["onCreateNode"] = ({
+  node,
+  getNode,
+  actions,
+}) => {
   const { createNodeField } = actions;
   if (node.internal.type === "Mdx") {
     const slug = createFilePath({
