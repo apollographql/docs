7752cf4a8d2062f33d7ed6ed3b13129656ac62a6
Trevor Blades <tdblades@gmail.com>
2023-05-15 10:31:56 -0700

    Direct non-logged in users to the signup page and add event tracking to the button

--- src/components/Header/StudioButton.js
+++ src/components/Header/StudioButton.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import {Button} from '@chakra-ui/react';
 import {FiArrowRight} from 'react-icons/fi';
+import {useUser} from '../../utils';
 
 export default function StudioButton() {
+  const {user} = useUser();
   return (
     <Button
       flexShrink={0}
@@ -14,10 +16,15 @@ export default function StudioButton() {
       rightIcon={<FiArrowRight />}
       as="a"
       ml="2"
-      href="https://studio.apollographql.com?referrer=docs"
+      href={`https://studio.apollographql.com${
+        user ? '' : '/signup'
+      }?referrer=docs`}
       target="_blank"
       rel="noopener noreferrer"
       d={{base: 'none', lg: 'flex'}}
+      onClick={() => {
+        window.gtag?.('event', 'studio_button_click');
+      }}
     >
       Launch GraphOS Studio
     </Button>
