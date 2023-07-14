7be6ed3ea8d828cbb833297a35d51373fabcf5b0
fernando-apollo <fernando@apollographql.com>
2023-07-14 11:13:16 -0300

    embedeable sandbox

---
+++ packages/chakra-helpers/src/EmbeddableSandbox.tsx
@@ -0,0 +1,50 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import {ApolloSandbox} from '@apollo/sandbox/react';
+import {Box, useColorMode} from '@chakra-ui/react';
+import {outdent} from 'outdent';
+
+type EmbeddableSandboxProps = {
+  initialEndpoint?: string;
+  document?: string;
+  height?: number;
+};
+
+export const EmbeddableSandbox = ({
+  initialEndpoint = 'http://localhost:4000',
+  document = outdent`
+    query MyQuery {
+    }
+  `,
+  height = 450
+}: EmbeddableSandboxProps): JSX.Element => {
+  const {colorMode} = useColorMode();
+  return (
+    <Box
+      h={height}
+      rounded="md"
+      sx={{
+        '.embed': {
+          boxSize: 'full'
+        }
+      }}
+    >
+      <ApolloSandbox
+        // give the component a key or else multiple explorers get rendered when
+        // the color mode changes
+        key={colorMode}
+        className="embed"
+        initialEndpoint={initialEndpoint}
+        initialState={{
+          document
+        }}
+      />
+    </Box>
+  );
+};
+
+EmbeddableSandbox.propTypes = {
+  initialEndpoint: PropTypes.string,
+  document: PropTypes.string,
+  height: PropTypes.number
+};
