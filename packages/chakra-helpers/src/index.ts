d14dfd05461a32e174fc6dc935b2d5b91267fb14
Trevor Blades <tdblades@gmail.com>
2023-07-18 11:56:03 -0700

    Remove chakra helpers from this repo

--- packages/chakra-helpers/src/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './helpers';
-export {defaultConfig as footerConfig} from './footer';
-export {EmbeddableExplorer} from './EmbeddableExplorer';
-export {EmbeddableSandbox} from './EmbeddableSandbox';
-export {MarkdownCodeBlock, CodeBlock, LineNumbersContext} from './CodeBlock';
-export {MultiCodeBlock, MultiCodeBlockContext} from './MultiCodeBlock';
