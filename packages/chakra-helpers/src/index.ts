f9b31ff54fc09e0b7e2c03b6007745e6b2932b75
fernando-apollo <108417792+fernando-apollo@users.noreply.github.com>
2023-07-17 21:36:29 -0300

    embedeable sandbox (#500)

--- packages/chakra-helpers/src/index.ts
+++ packages/chakra-helpers/src/index.ts
@@ -1,5 +1,6 @@
 export * from './helpers';
 export {defaultConfig as footerConfig} from './footer';
 export {EmbeddableExplorer} from './EmbeddableExplorer';
+export {EmbeddableSandbox} from './EmbeddableSandbox';
 export {MarkdownCodeBlock, CodeBlock, LineNumbersContext} from './CodeBlock';
 export {MultiCodeBlock, MultiCodeBlockContext} from './MultiCodeBlock';
