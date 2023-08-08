2e8bca523b970dc130306b81c0ac7147e7086bb6
Trevor Blades <tdblades@gmail.com>
2023-08-08 16:11:25 -0700

    Provide shared content as shortcodes

---
+++ src/content/shared/index.js
@@ -0,0 +1,16 @@
+export {default as CalloutCard} from './callout-card.mdx';
+export {default as ClientPQDifferences} from './client-pq-differences.mdx';
+export {default as ClientPQImplementation} from './client-pq-implementation.mdx';
+export {default as ClientPQIntro} from './client-pq-intro.mdx';
+export {default as Disclaimer} from './disclaimer.mdx';
+export {default as FeatureInPreview} from './feature-in-preview.mdx';
+export {default as GraphOSEnterpriseRequired} from './graphos-enteprise-required.mdx';
+export {default as ObtainGraphApiKey} from './obtain-graph-api-key.mdx';
+export {default as ObtainPersonalApiKey} from './obtain-personal-api-key.mdx';
+export {default as PQIntro} from './pq-intro.mdx';
+export {default as PQRouterConfiguration} from './pq-router-configuration.mdx';
+export {default as PQSecurityLevels} from './pq-security-levels.mdx';
+export {default as PublishPQMs} from './publish-pqms.mdx';
+export {default as RegisterCLI} from './register-cli.mdx';
+export {default as RegisterFederatedCLI} from './register-federated-cli.mdx';
+export {default as TopLevelAwait} from './top-level-await.mdx';
