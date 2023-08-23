6c9a6005af9b009f6cb6586fc3da64d0bda8f2e2
Maria Elisabeth Schreiber <maria.schreiber@apollographql.com>
2023-08-23 09:34:04 -0600

    Add PreviewFeature component

--- src/components/Page.js
+++ src/components/Page.js
@@ -57,6 +57,7 @@ import {
   PageContent,
   PageSeo
 } from './PageLayout';
+import {PreviewFeature} from './PreviewFeature';
 import {SiDiscord} from 'react-icons/si';
 import {TOTAL_HEADER_HEIGHT} from './Header';
 import {YouTube} from './YouTube';
@@ -186,7 +187,8 @@ const mdxComponents = {
   TypescriptApiBox: TypeScriptApiBox,
   EmbeddableExplorer,
   ButtonLink,
-  MinVersion
+  MinVersion,
+  PreviewFeature
 };
 
 const {processSync} = rehype()
