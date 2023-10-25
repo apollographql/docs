3d440edf5c486fe9058da8d7126066dafa1c4a8d
Maria Elisabeth Schreiber <maria.schreiber@apollographql.com>
2023-10-25 15:18:06 -0600

    Add icon to Preview component

--- src/components/PreviewFeature.js
+++ src/components/PreviewFeature.js
@@ -1,11 +1,14 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import {Box, Link} from '@chakra-ui/react';
+import {Box, Center, Flex, Link, Text, chakra} from '@chakra-ui/react';
+import {IoPartlySunnyOutline} from 'react-icons/io5';
 
-export const PreviewFeature = ({discordLink}) => {
+export const PreviewFeature = ({
+  discordLink = 'https://discord.gg/yFZJH2QYrK'
+}) => {
   return (
     <Box
-      pl="4"
+      pl="2"
       py="1"
       borderLeftWidth="2px"
       borderColor="primary"
@@ -18,20 +21,32 @@ export const PreviewFeature = ({discordLink}) => {
         }
       }}
     >
-      This feature is currently in{' '}
-      <Link
-        color={'primary'}
-        href="https://www.apollographql.com/docs/resources/product-launch-stages#preview"
-      >
-        preview
-      </Link>
-      . Your questions and feedback are highly valued{'—'}don&apos;t hesitate to
-      get in touch with your Apollo contact or on the official
-      <Link color={'primary'} href={discordLink}>
-        {' '}
-        Apollo GraphQL Discord
-      </Link>
-      .
+      <Flex as="span">
+        <chakra.span pl="10px" pr="10px">
+          <Center h="100%">
+            <IoPartlySunnyOutline />
+          </Center>
+        </chakra.span>
+        <Text pl="1">
+          <b>
+            This feature is currently in{' '}
+            <Link
+              color={'primary'}
+              href="https://www.apollographql.com/docs/resources/product-launch-stages#preview"
+            >
+              preview
+            </Link>
+            .
+          </b>{' '}
+          Your questions and feedback are highly valued{'—'}don&apos;t hesitate
+          to get in touch with your Apollo contact or on the official
+          <Link color={'primary'} href={discordLink}>
+            {' '}
+            Apollo GraphQL Discord
+          </Link>
+          .
+        </Text>
+      </Flex>
     </Box>
   );
 };
