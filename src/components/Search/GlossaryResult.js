900b2420bfc6f51e1e3c4c7f1e66f1743e5a9476
Maria Elisabeth Schreiber <maria.schreiber@apollographql.com>
2024-01-04 09:59:32 -0700

    Add glossary terms to search modal (#684)
    * Add unstyled Apollopedia result
    
    * Use markdown for term definition
    
    * Add icon
    
    * Add highlighting
    
    * Make GlossaryResult component
    
    * Add styling
    
    * Filter results to only show external terms without typos
    
    * Use definitions without examples
    
    * Apply style suggestions
    
    * Remove center align in search modal preview
    
    * Style preview
    
    * Display shortest term definition
    
    * Use function to strip code blocks
    
    * Tweak code stripping function
    
    * Simplify stripping function
    
    * Update iOS docset name
    
    * Remove Apollopedia results from Panel
    
    * Show 'tech notes' not 'technote docs' in preview panel
    
    * Only remove final sentences in stripping function
    
    * Update glossary URL

---
+++ src/components/Search/GlossaryResult.js
@@ -0,0 +1,103 @@
+import Highlight from './Highlight';
+import InlineCode from '../InlineCode';
+import Markdown from 'react-markdown';
+import PropTypes from 'prop-types';
+import React from 'react';
+import {Box, Flex, Heading, Icon, Text} from '@chakra-ui/react';
+import {BsJournals} from 'react-icons/bs';
+import {PrimaryLink} from '../RelativeLink';
+
+const DefinitionText = ({children}) => {
+  return (
+    <Box
+      py="2"
+      color="gray.500"
+      _dark={{
+        color: 'gray.400'
+      }}
+    >
+      <Text>{children}</Text>
+    </Box>
+  );
+};
+
+function stripCodeExample(definition) {
+  const hasCodeBlocks = /```[\s\S]*?```/g.test(definition);
+
+  if (hasCodeBlocks) {
+    // Find the index of the first occurrence of the code block
+    const codeBlockIndex = definition.indexOf('```');
+
+    if (codeBlockIndex !== -1) {
+      // Capture the text before the code block
+      const beforeCodeBlock = definition.substring(0, codeBlockIndex);
+
+      // Remove everything after the code block
+      definition = beforeCodeBlock.replace(/(\n|^)\s*$/, '');
+    }
+
+    // Remove the last sentence if it contains "example" or "shown below"
+    const sentences = definition.split('.').map(sentence => sentence.trim());
+    const filteredSentences = sentences
+      .filter(sentence => sentence !== '')
+      .filter(
+        (sentence, i, arr) =>
+          i !== arr.length - 1 ||
+          (!sentence.includes('example') && !sentence.includes('shown below'))
+      );
+
+    definition = filteredSentences.join('. ').trim();
+    if (!definition.endsWith('.')) {
+      definition = definition += '.';
+    }
+  }
+
+  return definition;
+}
+
+export default function GlossaryResult({item}) {
+  return (
+    <Box key="Apollopedia" borderBottomWidth="1px" pb="16px">
+      <Flex align="center" p="2" pr="0">
+        <Heading size="sm">Glossary</Heading>
+        <Box borderBottomWidth="1px" flexGrow="1" ml="2" />
+      </Flex>
+      <Flex p="2">
+        <Box mr="3" fontSize="xl" color="primary" flexShrink="0">
+          <Icon as={BsJournals} />
+        </Box>
+        <Box lineHeight="shorter" w="0" flexGrow="1">
+          <Box fontSize="lg">
+            <Highlight value={item._highlightResult.term.value} />
+          </Box>
+          <Markdown
+            components={{
+              p: DefinitionText,
+              a: PrimaryLink,
+              code: InlineCode
+            }}
+          >
+            {stripCodeExample(item.definition)}
+          </Markdown>
+          <PrimaryLink
+            aria-label="Go to the glossary"
+            as="a"
+            href={`https://www.apollographql.com/docs/resources/glossary#${item.term
+              .replace(/\s+/g, '-')
+              .toLowerCase()}`}
+            mt="12px"
+            style={{display: 'flex', alignItems: 'center'}}
+          >
+            {item.definition === stripCodeExample(item.definition)
+              ? 'Go to the glossary ⏎'
+              : 'Explore an example in the glossary ⏎'}
+          </PrimaryLink>
+        </Box>
+      </Flex>
+    </Box>
+  );
+}
+
+GlossaryResult.propTypes = {
+  item: PropTypes.object.isRequired
+};
