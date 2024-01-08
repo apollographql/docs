c7ef61feaaeb8f77efe2835889c77d9798057fd5
Andrew McGivery <andrew.mcgivery@apollographql.com>
2024-01-08 15:09:20 -0500

    Cleanup

--- src/components/TechNotes/index.js
+++ src/components/TechNotes/index.js
@@ -1,7 +1,3 @@
 export { AllNotes } from "./AllNotes";
-export { AllNotesRecents } from "./AllNotesRecents";
-export { AllNotesRecentsInline } from "./AllNotesRecentsInline";
 export { NotesList } from "./NotesList";
 export { TagList } from "./TagList";
-export { RecentlyUpdatedNotes } from "./RecentlyUpdatedNotes";
-export { RecentlyAddedNotes } from "./RecentlyAddedNotes";
