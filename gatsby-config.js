2cfb50615c456da168d22a5f874dc895facf4142
Trevor Blades <tdblades@gmail.com>
2024-01-16 18:30:34 -0800

    Use original font loading strat

--- gatsby-config.js
+++ gatsby-config.js
@@ -87,15 +87,6 @@ const plugins = [
             family: 'Fira Code',
             variants: ['400', '600']
           }
-        ],
-        selfHosted: [
-          {
-            family: 'Aeonik',
-            urls: {
-              woff2: '/src/fonts/aeonik/Aeonik-Medium.woff2'
-            },
-            fontWeight: 500
-          }
         ]
       }
     }
