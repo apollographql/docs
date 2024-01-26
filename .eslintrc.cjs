1bf8fd09bf149765f17bf397b8de45886a47303d
Trevor Blades <tdblades@gmail.com>
2024-01-26 00:11:25 -0800

    Add some more stuff

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,5 +11,22 @@ module.exports = {
     "plugin:@typescript-eslint/recommended",
     "plugin:prettier/recommended",
     "plugin:mdx/recommended",
+    "plugin:astro/recommended",
+  ],
+  plugins: ["simple-import-sort"],
+  rules: {
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
+    "@typescript-eslint/consistent-type-imports": "warn",
+  },
+  overrides: [
+    {
+      files: ["*.astro"],
+      parser: "astro-eslint-parser",
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        extraFileExtensions: [".astro"],
+      },
+    },
   ],
 };
