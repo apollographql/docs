b200f9e884533bd055261561e8f08f671fec3fb3
Trevor Blades <tdblades@gmail.com>
2024-01-26 00:44:43 -0800

    Add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import mdx from "@astrojs/mdx";
+import netlify from "@astrojs/netlify";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx(), tailwind()],
+  output: "hybrid",
+  adapter: netlify(),
 });
