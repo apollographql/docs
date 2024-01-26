9999a62338ae735bbd0165a8e7a167569781da5a
Trevor Blades <tdblades@gmail.com>
2024-01-26 08:59:27 -0800

    Noodle on a local dev story

--- prebuild.ts
+++ prebuild.ts
@@ -2,20 +2,20 @@ import { deleteAsync } from "del";
 import { downloadTemplate } from "giget";
 
 const config = {
-  "apollographql/apollo-client/docs/source": "react",
-  // "apollographql/apollo-client/docs/source#version-2.6": "react/v2",
-  // "apollographql/apollo-server/docs/source": "apollo-server",
-  // "apollographql/apollo-server/docs/source#version-3": "apollo-server/v3",
-  // "apollographql/apollo-server/docs/source#version-2": "apollo-server/v2",
-  // "apollographql/apollo-kotlin/docs/source": "kotlin",
-  // "apollographql/apollo-ios-dev/docs/source": "ios",
-};
+  react: "apollographql/apollo-client/docs/source",
+  "react/v2": "apollographql/apollo-client/docs/source#version-2.6",
+  "apollo-server": "apollographql/apollo-server/docs/source",
+  "apollo-server/v3": "apollographql/apollo-server/docs/source#version-3",
+  "apollo-server/v2": "apollographql/apollo-server/docs/source#version-2",
+  kotlin: "apollographql/apollo-kotlin/docs/source",
+  ios: "apollographql/apollo-ios-dev/docs/source",
+} satisfies Record<string, string>;
 
 // copy each docset's source files into the docs content folder
 await Promise.all(
-  Object.entries(config).map(([from, to]) =>
-    downloadTemplate(from, {
-      dir: to,
+  Object.entries(config).map(([path, repo]) =>
+    downloadTemplate(repo, {
+      dir: path,
       provider: "github",
       force: true,
       cwd: "src/content/docs",
@@ -23,5 +23,5 @@ await Promise.all(
   ),
 );
 
-// delete all files that astro might consider data
+// delete all files that astro might consider "data"
 await deleteAsync("src/content/docs/**/*.{json,yaml,yml}");
