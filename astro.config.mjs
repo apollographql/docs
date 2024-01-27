1ad3ad8298e05aed0609dd6860dba2d287ff66a6
Trevor Blades <tdblades@gmail.com>
2024-01-26 20:21:55 -0800

    Add sidebar and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import mdx from "@astrojs/mdx";
 import netlify from "@astrojs/netlify";
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), tailwind(), react()],
+  integrations: [mdx(), tailwind(), react(), sitemap()],
   output: "hybrid",
   adapter: netlify(),
   vite: {
