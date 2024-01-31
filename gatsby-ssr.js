4aa2311d89722528c5ae10ff2a88d2d298d27748
Maria Elisabeth Schreiber <maria.schreiber@apollographql.com>
2024-01-31 09:35:29 -0700

    Remove Freddy script and feedback button (#737)

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -13,18 +13,6 @@ export const wrapPageElement = ({element, props}) => (
 
 const headComponents = [
   <script
-    key="ff"
-    dangerouslySetInnerHTML={{
-      __html: `
-          var ffWidgetId = '3131c43c-bfb5-44e6-9a72-b4094f7ec028';
-          var ffWidgetScript  = document.createElement('script');
-          ffWidgetScript.type = 'text/javascript';
-          ffWidgetScript.src = 'https://freddyfeedback.com/widget/freddyfeedback.js';
-          document.head.appendChild(ffWidgetScript);
-        `
-    }}
-  />,
-  <script
     key="extend-fix"
     dangerouslySetInnerHTML={{
       __html: `
