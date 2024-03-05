84f363358684a6f16de355cf4f7e13e80247954b
Maria Elisabeth Schreiber <maria.schreiber@apollographql.com>
2024-03-05 16:46:09 -0700

    Add exception

--- src/components/RelativeLink.js
+++ src/components/RelativeLink.js
@@ -49,7 +49,10 @@ function useLinkProps(href) {
     const url = new URL(href);
     if (
       url.host === window.location.host &&
-      href.startsWith('https://www.apollographql.com/docs/')
+      href.startsWith('https://www.apollographql.com/docs/') &&
+      !href.startsWith(
+        'https://www.apollographql.com/docs/ios/docc/documentation/'
+      )
     ) {
       href = url.pathname;
     }
