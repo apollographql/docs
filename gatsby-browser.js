feac4fa87634620b9b4c027ed7d0bfc999c0fc2c
Trevor Blades <tdblades@gmail.com>
2024-03-13 12:48:59 -0700

    Use fontsource instead of gatsby-plugin-webfonts (#804)

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,9 @@
+import '@fontsource/fira-code/400.css';
+import '@fontsource/fira-code/600.css';
+import '@fontsource/inter/400.css';
+import '@fontsource/inter/600.css';
+import '@fontsource/inter/700.css';
+
 import {withPrefix} from 'gatsby';
 export {wrapPageElement} from './gatsby-ssr';
 
