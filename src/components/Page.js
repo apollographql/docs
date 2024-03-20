a408e0c0585e918d7a19e7a38f9485937a0f5fc7
Daniel Abdelsamed <daniel@abdelsamed.com>
2024-03-20 16:09:22 -0400

    Customized styles

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,4 +1,4 @@
-import 'react-medium-image-zoom/dist/styles.css';
+import '../assets/image-zoom.css';
 import * as sharedContent from '../content/shared';
 import Blockquote from './Blockquote';
 import CodeColumns from './CodeColumns';
