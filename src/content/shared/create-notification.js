68381af6c2fbd94c8c624ccbff245a2438f5e7d9
Maria Elisabeth Schreiber <maria.schreiber@apollographql.com>
2024-04-25 08:34:26 -0600

    Copyedits

--- src/content/shared/create-notification.js
+++ src/content/shared/create-notification.js
@@ -1,50 +1,47 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import RelativeLink from '../../components/RelativeLink';
-import {ListItem, OrderedList, Text} from '@chakra-ui/react';
+import {ListItem, OrderedList} from '@chakra-ui/react';
 
 export const CreateNotification = ({notificationType}) => {
   return (
-    <>
-      <Text>
+    <OrderedList spacing="4">
+      <ListItem>
         Go to your graph&apos;s <strong>Settings</strong> page in{' '}
         <RelativeLink href="https://studio.apollographql.com/?referrer=docs-content">
           GraphOS Studio
         </RelativeLink>
         .
-      </Text>
-      <OrderedList spacing="4">
+      </ListItem>
+      <ListItem>
+        Open the <strong>Reporting</strong> tab.
+      </ListItem>
+      <ListItem>
+        Click <strong>Add notification</strong> in the upper right.
+      </ListItem>
+      <ListItem>
+        Select <strong>{notificationType}</strong> and click{' '}
+        <strong>Next</strong>.
+      </ListItem>
+      <ListItem>
+        In the dropdown, select which graph variant you want to receive
+        notifications for.
+      </ListItem>
+      <ListItem>
+        Select an existing channel to send notifications to, or select which
+        type of new channel you want to configure. Click <strong>Next</strong>.
+      </ListItem>
+      <ListItem>
+        If you&apos;re configuring a new channel, complete the steps in the next
+        section.
+      </ListItem>
+      {notificationType === 'Daily report' && (
         <ListItem>
-          Select the <strong>Reporting</strong> tab.
+          Select a time zone. GraphOS sends the report daily at 9 AM in the
+          selected time zone.
         </ListItem>
-        <ListItem>
-          Click <strong>Add notification</strong> in the upper right.
-        </ListItem>
-        <ListItem>
-          Select <strong>{notificationType}</strong> and click{' '}
-          <strong>Next</strong>.
-        </ListItem>
-        <ListItem>
-          In the dropdown, select which graph variant you want to receive
-          notifications for.
-        </ListItem>
-        <ListItem>
-          Select an existing channel to send notifications to, or select which
-          type of new channel you want to configure. Click <strong>Next</strong>
-          .
-        </ListItem>
-        <ListItem>
-          If you&apos;re configuring a new channel, complete the steps in the
-          next section.
-        </ListItem>
-        {notificationType === 'Daily report' && (
-          <ListItem>
-            Select a time zone. The report is sent daily at 9 AM in the selected
-            time zone.
-          </ListItem>
-        )}
-      </OrderedList>
-    </>
+      )}
+    </OrderedList>
   );
 };
 
