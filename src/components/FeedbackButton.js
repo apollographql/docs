33eea7583acbb07354deef1f65e0d162183c96cb
Maria Elisabeth Schreiber <maria.schreiber@apollographql.com>
2024-06-03 17:14:59 -0600

    Update modal size

--- src/components/FeedbackButton.js
+++ src/components/FeedbackButton.js
@@ -7,10 +7,6 @@ import {Text} from '@chakra-ui/react';
 export const FeedbackButton = ({title}) => {
   return (
     <PopupButton
-      hidden={{
-        path: window.location.pathname,
-        title
-      }}
       id="miEpZmDw/"
       as="a"
       style={{
@@ -23,6 +19,11 @@ export const FeedbackButton = ({title}) => {
         padding: 0
       }}
       className="chakra-button css-8luegd"
+      hidden={{
+        path: window.location.pathname,
+        title
+      }}
+      size={60}
     >
       <span className="chakra-button__icon css-1wh2kri">
         <StarIcon />
