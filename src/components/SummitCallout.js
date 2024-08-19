042b05728d90e937f994ddae80a57e262823b67c
Edward Huang <edward.huang@apollographql.com>
2024-08-19 10:18:06 -0700

    support summit panel name (#923)

--- src/components/SummitCallout.js
+++ src/components/SummitCallout.js
@@ -1,11 +1,16 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import {Box, Flex, Icon, Text} from '@chakra-ui/react';
 import RelativeLink from './RelativeLink';
-import {SummitIcon} from './Icons'
+import {Box, Flex, Icon, Text} from '@chakra-ui/react';
+import {SummitIcon} from './Icons';
 
-export const SummitCallout = ({topic, workshopName, URL, ...props}) => {
-  console.log(SummitIcon)
+export const SummitCallout = ({
+  topic,
+  workshopName,
+  panelName,
+  URL,
+  ...props
+}) => {
   return (
     <Box
       pl="4"
@@ -22,18 +27,30 @@ export const SummitCallout = ({topic, workshopName, URL, ...props}) => {
       {...props}
     >
       <Flex align="center">
-        <Icon as={SummitIcon} mr={1}/>
+        <Icon as={SummitIcon} mr={1} />
         <Text>
           <strong>Want to learn about {topic} in-person?</strong>
         </Text>
       </Flex>
-      <Text>Don't miss the <RelativeLink href={URL+'?utm_campaign=docs_content_callouts&utm_medium=website&utm_source=docs'}>{workshopName}</RelativeLink> workshop at this year's GraphQL Summit.</Text>
+      <Text>
+        Don't miss the{' '}
+        <RelativeLink
+          href={
+            URL +
+            '?utm_campaign=docs_content_callouts&utm_medium=website&utm_source=docs'
+          }
+        >
+          {workshopName ? workshopName : panelName}
+        </RelativeLink>{' '}
+        {workshopName ? 'workshop' : 'panel'} at this year's GraphQL Summit.
+      </Text>
     </Box>
   );
 };
 
 SummitCallout.propTypes = {
   topic: PropTypes.string.isRequired,
-  workshopName: PropTypes.string.isRequired,
+  workshopName: PropTypes.string,
+  panelName: PropTypes.string,
   URL: PropTypes.string.isRequired
 };
